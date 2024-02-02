7aaf17af0589ee16f6272a4856e7768022968f6e
yisus82 <djalma8@gmail.com>
2024-02-02 09:00:28 +0100

    Added tools

--- code/player.py
+++ code/player.py
@@ -1,7 +1,10 @@
 from os import path
+from random import choice
+
 import pygame
 
 from utils import import_folder
+from timer import Timer
 
 
 class Player(pygame.sprite.Sprite):
@@ -19,37 +22,73 @@ class Player(pygame.sprite.Sprite):
         self.speed = 5.0
         self.direction = pygame.math.Vector2()
         self.position = pygame.math.Vector2(self.rect.center)
+        self.tools = ['axe', 'hoe', 'water']
+        self.selected_tool = None
+        self.timers = {
+            'use_tool': Timer(500, self.use_tool),
+        }
 
     def import_animations(self):
         player_folder = path.join('..', 'graphics', 'player')
         self.animations = {
-            'up': [],
-            'up_idle': [],
             'down': [],
+            'down_axe': [],
+            'down_hoe': [],
             'down_idle': [],
+            'down_water': [],
             'left': [],
+            'left_axe': [],
+            'left_hoe': [],
             'left_idle': [],
+            'left_water': [],
             'right': [],
+            'right_axe': [],
+            'right_hoe': [],
             'right_idle': [],
+            'right_water': [],
+            'up': [],
+            'up_axe': [],
+            'up_hoe': [],
+            'up_idle': [],
+            'up_water': [],
         }
         for animation_name in self.animations.keys():
             animation_folder = path.join(player_folder, animation_name)
             self.animations[animation_name] = import_folder(animation_folder)
 
+    def update_timers(self):
+        for timer in self.timers.values():
+            timer.update()
+
     def input(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP] or keys[pygame.K_w]:
-            self.direction.y = -1
-        elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            self.direction.y = 1
-        else:
-            self.direction.y = 0
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.direction.x = 1
-        elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            self.direction.x = -1
-        else:
-            self.direction.x = 0
+        if not self.timers['use_tool'].active:
+            if keys[pygame.K_SPACE] and self.selected_tool is not None:
+                self.frame_index = 0
+                self.direction.x = 0
+                self.direction.y = 0
+                self.timers['use_tool'].activate()
+            else:
+                if keys[pygame.K_UP] or keys[pygame.K_w]:
+                    self.direction.y = -1
+                elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+                    self.direction.y = 1
+                else:
+                    self.direction.y = 0
+                if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+                    self.direction.x = 1
+                elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+                    self.direction.x = -1
+                else:
+                    self.direction.x = 0
+                if keys[pygame.K_1]:
+                    self.selected_tool = self.tools[0]
+                elif keys[pygame.K_2]:
+                    self.selected_tool = self.tools[1]
+                elif keys[pygame.K_3]:
+                    self.selected_tool = self.tools[2]
+                elif keys[pygame.K_0]:
+                    self.selected_tool = None
 
     def update_status(self):
         if self.direction.x == 0 and self.direction.y == 0:
@@ -62,6 +101,8 @@ class Player(pygame.sprite.Sprite):
             self.status = 'down'
         elif self.direction.y < 0:
             self.status = 'up'
+        if self.selected_tool is not None and self.timers['use_tool'].active:
+            self.status = self.status.split('_')[0] + '_' + self.selected_tool
 
     def move(self):
         if self.rect is not None:
@@ -74,6 +115,9 @@ class Player(pygame.sprite.Sprite):
             self.position.y += self.direction.y * self.speed
             self.rect.centery = self.position.y
 
+    def use_tool(self):
+        print(self.selected_tool, 'used')
+
     def animate(self):
         if self.status in self.animations:
             animation = self.animations[self.status]
@@ -84,6 +128,7 @@ class Player(pygame.sprite.Sprite):
             self.rect = self.image.get_rect(center=self.position)
 
     def update(self):
+        self.update_timers()
         self.input()
         self.update_status()
         self.move()
